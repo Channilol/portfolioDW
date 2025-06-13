44445aa15d495628e0fed52165ea90e43e2124b4
Channilol <fcannizzo97@gmail.com>
2025-06-13 17:27:57 +0200

    configurato json progetti

--- src/components/ProjectFrame/ProjectFrame.jsx
+++ src/components/ProjectFrame/ProjectFrame.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './ProjectFrame.css'
 
-export default function ProjectFrame() {
+export default function ProjectFrame({ title }) {
     return (
         <div className="projectFrame">
             <div className="projectContent">
-                <p>Test project</p>
+                <p className='projectTitle'>{title}</p>
             </div>
         </div>
     )
--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
 import './ProjectList.css'
 import ProjectFrame from '../ProjectFrame/ProjectFrame'
+import listaProgetti from '../../assets/projects.json'
 
 export default function ProjectList() {
     return (
         <div className="ProjectList">
-            <ProjectFrame></ProjectFrame>
-            <ProjectFrame></ProjectFrame>
-            <ProjectFrame></ProjectFrame>
-            <ProjectFrame></ProjectFrame>
-            <ProjectFrame></ProjectFrame>
+            {listaProgetti.progetti.map((progetto) => (
+                <ProjectFrame key={progetto['nome']} title={progetto['nome']} />
+            ))}
         </div>
     )
 }
