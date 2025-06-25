9b8b88f134e19a93b2c775c0a851d58c29f7be1b
Channilol <fcannizzo97@gmail.com>
2025-06-25 14:31:28 +0200

    sistemate le immagini dei progetti

--- src/components/ProjectFrame/ProjectFrame.jsx
+++ src/components/ProjectFrame/ProjectFrame.jsx
@@ -1,31 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './ProjectFrame.css'
-import Link from '../../assets/link_icon.svg'
+import LinkIcon from '../../assets/link_icon.svg'
 
-export default function ProjectFrame({ title, tech, url, image_url }) {
+export default function ProjectFrame({ title, tech, url, image }) {
     const [isInView, setIsInView] = useState(false)
     const frameRef = useRef(null)
 
     useEffect(() => {
         const observer = new IntersectionObserver(
             ([entry]) => {
-                // Il component è considerato "a metà schermo" quando almeno il 50% è visibile
                 setIsInView(entry.intersectionRatio >= 0.6)
             },
             {
-                threshold: [0, 0.25, 0.5, 0.75, 1], // Monitora diversi livelli di visibilità
-                rootMargin: '0px' // Nessun margine aggiuntivo
+                threshold: [0, 0.25, 0.5, 0.75, 1],
+                rootMargin: '0px'
             }
         )
-
-        if (frameRef.current) {
-            observer.observe(frameRef.current)
-        }
-
+        if (frameRef.current) observer.observe(frameRef.current)
         return () => {
-            if (frameRef.current) {
-                observer.unobserve(frameRef.current)
-            }
+            if (frameRef.current) observer.unobserve(frameRef.current)
         }
     }, [])
 
@@ -35,22 +28,36 @@ export default function ProjectFrame({ title, tech, url, image_url }) {
             className={`projectFrame ${isInView ? 'illuminated' : ''}`}
         >
             <div className="projectContent">
-                <p className='projectTitle'>{title}</p>
+                {/* Wrapper per l’immagine */}
+                <div className="projectImageWrapper">
+                    <img src={image} alt={title} className="projectImage" />
+                </div>
+
+                {/* Titolo */}
+                <p className="projectTitle">{title}</p>
+
+                {/* Tecnologie */}
                 <div className="projectTech">
                     {tech.map((t, i) => (
                         <span
                             key={i}
                             className="techItem"
-                            style={{ backgroundColor: t['color'] }}
+                            style={{ backgroundColor: t.color }}
                         >
-                            {t['title']}
+                            {t.title}
                         </span>
                     ))}
                 </div>
-                <a href={url} target='_blank' rel="noreferrer" className='projectLink' >
-                    <img src={Link} alt="Link" width={36} height={36} />
+
+                {/* Link al progetto */}
+                <a
+                    href={url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="projectLink"
+                >
+                    <img src={LinkIcon} alt="Link" width={36} height={36} />
                 </a>
-                {image_url != null ? <img src={image_url} alt="" srcSet="" /> : ''}
             </div>
         </div>
     )
