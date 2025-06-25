93b1baf71168e8e22df3880f232f29ec11e311d1
Channilol <fcannizzo97@gmail.com>
2025-06-25 13:54:08 +0200

    import SpeedInsight di Vercel

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import ProjectList from './components/ProjectList/ProjectList';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   const [selectedBox, setSelectedBox] = useState('work');
 
   return (
     <>
+      <SpeedInsights />
       <div className="App">
         <Header
           selectedBox={selectedBox}
           setSelectedBox={setSelectedBox}
         />
-        <ProjectList />
+        {selectedBox === 'work' ? <ProjectList /> : null}
       </div>
     </>
   );
